6dc17a4810a82daa96815f216580a1e19fad6c29
shawnmccarthy <shawn@mccarthysoftware.com>
2022-06-03 09:37:19 -0600

    code so far up to doctors get

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const app = express(); //creating an expres app
 const { PORT = 3000 } = process.env; //try to get to the PORT from environment variable else use 3000
 import bodyParser from "body-parser";
 import cors from "cors";
+import data from "./data.js";
 
 app.use(bodyParser.json()).use(cors())
 
 app.get("/", (request, response) =>
     response.send("Hello World, Waterloo!")
+);
+
+app.get("/api/v1/doctors", (req, res) => {
+        res.json(data.doctors);
+    }
+);
+
+app.get("/api/v1/doctors/:id", (req, res) => {
+        const id = parseInt(req.params.id);
+        const doctor = data.doctors.find((doc) => doc.id === id);
+        if (!doctor) {
+            res.status(404).json({error: "Doctor not found."});
+        }
+        return res.json(doctor);
+    }
 );
 
 app.get("/healthcheck", (req, res) => {
